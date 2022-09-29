e54b4f6f4472cc6945ce522929d2524e7a907682
javascript <94991700+franjoc@users.noreply.github.com>
2022-09-29 19:26:22 +0200

    Add files via upload

---
+++ src/ejemploUD2/201tresfrases.php
@@ -0,0 +1,26 @@
+
+<?php
+
+?>
+
+
+ echo "<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>";
+    $frase1="Hola que tal estais<br/>;
+    $frase2="todo correcto<br/>;
+    $frase3="Bueno pues hasta luego $frase2<br/>;
+    $frase4="No se escribir $frase1<br/>;
+?>
+<body>
+<?php
+    echo $frase2;
+?>
+
+</body>
+</html>
